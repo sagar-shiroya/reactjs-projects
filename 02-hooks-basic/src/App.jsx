9af3ec27243f8374a462948b17a337d32022ae19
Sagar Shiroya <sagar.shiroya8@gmail.com>
2026-05-03 13:08:19 +0530

    feat: 02 - added like button and toggle dark light mode button using useState

--- 02-hooks-basic/src/App.jsx
+++ 02-hooks-basic/src/App.jsx
@@ -18,8 +18,58 @@ function Counter({
 	);
 }
 
+function DarkLightModeToggle({ mode, setMode }) {
+	function toggleMode() {
+		const button = document.getElementById("toggleMode");
+		if (mode === "light") {
+			setMode("dark");
+			document.body.style.backgroundColor = "black";
+			document.body.style.color = "white";
+			button.textContent = "Switch to Light Mode";
+			button.style.color = "white";
+		} else {
+			setMode("light");
+			document.body.style.backgroundColor = "white";
+			document.body.style.color = "black";
+			button.textContent = "Switch to Dark Mode";
+			button.style.color = "black";
+		}
+	}
+	return (
+		<>
+			<button id="toggleMode" onClick={toggleMode}>
+				Switch to Light Mode
+			</button>
+		</>
+	);
+}
+
+function LikeButton({ like, setLike }) {
+	function toggleLike() {
+		const likeButton = document.getElementById("likeButton");
+		if (!like) {
+			likeButton.textContent = "❤️";
+			likeButton.style.backgroundColor = "red";
+		} else {
+			likeButton.textContent = "🤍";
+			likeButton.style.backgroundColor = "black";
+		}
+		setLike(!like);
+	}
+	return (
+		<>
+			<h1>Like Button</h1>
+			<button id="likeButton" onClick={toggleLike}>
+				🤍
+			</button>
+		</>
+	);
+}
+
 function App() {
 	const [counter, setCounter] = useState(0);
+	const [like, setLike] = useState(false);
+	const [mode, setMode] = useState("dark");
 
 	function incrementCounter() {
 		setCounter(counter + 1);
@@ -32,6 +82,7 @@ function App() {
 	function resetCounter() {
 		setCounter(0);
 	}
+
 	return (
 		<>
 			<Counter
@@ -40,6 +91,13 @@ function App() {
 				decrementCounter={decrementCounter}
 				resetCounter={resetCounter}
 			/>
+
+			<hr />
+
+			<LikeButton like={like} setLike={setLike} />
+
+			<hr />
+			<DarkLightModeToggle mode={mode} setMode={setMode} />
 		</>
 	);
 }
