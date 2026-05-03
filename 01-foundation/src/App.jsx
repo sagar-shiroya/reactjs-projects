6b7d21ef2ae370f50c9275801000f1c770632755
Sagar Shiroya <sagar.shiroya8@gmail.com>
2026-05-03 10:56:12 +0530

    feat: 01 - printing arrays, objects, date and variable in JSX

--- 01-foundation/src/App.jsx
+++ 01-foundation/src/App.jsx
@@ -2,11 +2,42 @@ import "./App.css";
 
 function App() {
 	const name = "Sagar";
+	const books = [
+		{
+			title: "Designing Data Intensive Applications",
+			author: "Martin Kleppman",
+			publisher: "O'reilly",
+		},
+		{
+			title: "Database Internals",
+			author: "Petrov",
+			publisher: "O'reilly",
+		},
+	];
+
+	const genres = ["technical", "fictional", "non-fictional"];
+
 	return (
-		<div>
-			<h1>React Course</h1>
-			<p>Welcome {name}</p>
-		</div>
+		<section>
+			<h1>Favorite Books of {name}</h1>
+			<span>Date: {new Date().toString()}</span>
+			<p>
+				Genres:{" "}
+				{genres.reduce(
+					(str, currentVal) => (str += currentVal + ", "),
+					"",
+				)}
+			</p>
+			<div className="books">
+				{books.map((book) => (
+					<article>
+						<h2>{book.title}</h2>
+						<p>Author: {book.author}</p>
+						<span>Published by {book.publisher}</span>
+					</article>
+				))}
+			</div>
+		</section>
 	);
 }
 
