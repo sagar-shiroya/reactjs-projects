9ba687350c971c8205a69816694db1235111bbcc
Sagar Shiroya <sagar.shiroya8@gmail.com>
2026-05-03 12:08:46 +0530

    fix: created seperate Counter component

--- 02-hooks-basic/src/App.jsx
+++ 02-hooks-basic/src/App.jsx
@@ -2,27 +2,44 @@ import { useState } from "react";
 
 import "./App.css";
 
-function App() {
-	const [counter, setcounter] = useState(0);
-
-	function incrementCounter() {
-		setcounter(counter + 1);
-	}
-
-	function decrementCounter() {
-		setcounter(counter - 1);
-	}
-
-	function resetCounter() {
-		setcounter(0);
-	}
-
+function Counter({
+	counter,
+	incrementCounter,
+	decrementCounter,
+	resetCounter,
+}) {
 	return (
 		<>
 			<h1>Counter: {counter}</h1>
 			<button onClick={incrementCounter}>+</button>
 			<button onClick={decrementCounter}>-</button>
 			<button onClick={resetCounter}>Reset</button>
+		</>
+	);
+}
+
+function App() {
+	const [counter, setCounter] = useState(0);
+
+	function incrementCounter() {
+		setCounter(counter + 1);
+	}
+
+	function decrementCounter() {
+		setCounter(counter - 1);
+	}
+
+	function resetCounter() {
+		setCounter(0);
+	}
+	return (
+		<>
+			<Counter
+				counter={counter}
+				incrementCounter={incrementCounter}
+				decrementCounter={decrementCounter}
+				resetCounter={resetCounter}
+			/>
 		</>
 	);
 }
